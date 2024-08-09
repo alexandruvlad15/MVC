2d2c64f9c0f8da68080ae0130386427b3d24b74a
alexandruvlad15 <alexandru.vlad1504@stud.aero.upb.ro>
2024-08-09 13:50:53 +0300

    Controller no comments 2

--- Controller.go
+++ Controller.go
@@ -1,1 +1,14 @@
 package Controller
+
+import (
+	"Part2/Model"
+)
+
+func DetermineSolutions(rho, S, P, G, Cx0, k, eta, Cz, Cx, Czmax float64) (v, gamma, Vmax, Gammamax float64) {
+	solutions := Model.Optimize(rho, S, P, G, Cx0, k, eta, Cz, Cx, Czmax)
+	v = solutions[0]
+	gamma = solutions[1]
+	Vmax = Model.CalcVmax(G, rho, S, Czmax)
+	Gammamax = Model.CalcGammamax(P, eta, rho, S, Vmax, Cx0, k, Czmax, G)
+	return
+}
