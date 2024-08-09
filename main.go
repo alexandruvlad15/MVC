83974ed60564c5def97312ac7d8ca8887d4d085d
alexandruvlad15 <alexandru.vlad1504@stud.aero.upb.ro>
2024-08-09 13:52:41 +0300

    main.go no comments

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"Part2/Controller"
+	"Part2/Views"
+)
+
+func main() {
+	// Citim valorile de intrare folosind pachetul View
+	rho, G, S, P, Cx0, eta, k, Cz, Czmax, Cx := Views.Read()
+
+	// Determinăm soluțiile folosind controller-ul
+	v, gamma, Vmax, Gammamax := Controller.DetermineSolutions(rho, S, P, G, Cx0, k, eta, Cz, Cx, Czmax)
+
+	// Afișăm rezultatele folosind View
+	Views.DispRes(nil, rho, S, P, G, Cx0, k, eta, Cz, Cx, v, gamma, Vmax, Gammamax)
+}
