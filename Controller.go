18d49638da12720e382c70d295b0e4d38ab452ed
alexandruvlad15 <alexandru.vlad1504@stud.aero.upb.ro>
2024-08-10 17:10:47 +0300

    Controller with comments

--- Controller.go
+++ Controller.go
@@ -1,11 +1,11 @@
 package Controller
 
 import (
-	"Part2/Model"
+	"Part2/Model" //pentru a utiliza algoritmul de determinare a parametrilor
 )
 
-func DetermineSolutions(rho, S, P, G, Cx0, k, eta, Cz, Cx, Czmax float64) (v, gamma, Vmax, Gammamax float64) {
-	solutions := Model.Optimize(rho, S, P, G, Cx0, k, eta, Cz, Cx, Czmax)
+func DetermineSolutions(rho, S, P, G, Cx0, k, eta, Cz, Cx, Czmax float64) (v, gamma, Vmax, Gammamax float64) { //functie ce determina valorile celor 4 parametri
+	solutions := Model.Optimize(rho, S, P, G, Cx0, k, eta, Cz, Cx, Czmax) //functia de optimizare, ce determina vectorul de solutii pentru v si gamma
 	v = solutions[0]
 	gamma = solutions[1]
 	Vmax = Model.CalcVmax(G, rho, S, Czmax)
